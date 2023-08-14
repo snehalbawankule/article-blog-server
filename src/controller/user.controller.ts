92d72495d6da4aae84b69a0ce094a5cb2a1967a4
snehalbawankule <snehalbawankule2000@gmail.com>
2023-08-14 12:39:47 +0530

    commit

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -24,18 +24,16 @@ const addUser = async (req: any, res: any) => {
 const checkUser = async (req: any, res: any) => {
   try {
     const { email, password } = req.body;
-    console.log(password);
     const user = await userCheck(email);
     if (!user) {
       return res.send({ message: "User not found" });
     }
-    console.log(user);
-    const isMatch = await bcrypt.compare(password, user.dataValues.password);
-    console.log(password, user.dataValues.password);
+    const isMatch = await bcrypt.compare(password, user?.dataValues.password);
+    console.log(password, user?.dataValues.password);
     if (isMatch) {
-      return res.status(200).send(user);
+      return res.send({ message: "Invalid password" });
     }
-    return res.send({ message: "Invalid password" });
+    return res.status(200).send(user);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server Error" });
